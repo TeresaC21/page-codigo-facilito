42d1e76a4f966ed38a9e573796624fcb5428130c
Teresa Carbajal <tezzi.cah@gmail.com>
2021-02-21 23:56:57 -0600

    Setup base function js for time show animation gift on header

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ window.onload = () => {
   containerSpinner.style.visibility = "hidden";
   containerSpinner.style.opacity = "0";
 };
+
+/* *************** ENTER RIGHT TO LEFT GIFT *************** */
+moveImage.style.visibility = "hidden";
+setTimeout(() => {
+  moveImage.style.visibility = "visible";
+  moveImage.classList.add("move-right-to-left");
+}, 2000);
