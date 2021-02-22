de6a59f8bb85a0f60824e03412e03493a2017473
Teresa Carbajal <tezzi.cah@gmail.com>
2021-02-22 02:25:43 -0600

    Created for funtionality modal like open, close with the events

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 
 let moveImage = document.querySelector("#moveImage");
 let containerSpinner = document.querySelector("#container-spinner");
-
-// -- EVENTS --
-
-document.addEventListener("scroll", () => {
-  isTypeTime("2");
-  isTypeTime("3");
-  isTypeTime("4");
-  isTypeTime("5");
-});
+let modalContainer = document.querySelector(".modal-container");
+let modal = document.querySelector(".modal");
+let open = document.querySelector(".open");
+let close = document.querySelector(".close");
 
 // -- FUNCTIONS --
 
@@ -28,17 +23,15 @@ setTimeout(() => {
 }, 2000);
 
 /* *************** SCROLL SECTION SERVICES *************** */
-
-const isTypeTime = (number) => {
-  let moveScroll = document.querySelectorAll(`.cards-${number}`);
+const isTypeTime = (second) => {
+  let moveScroll = document.querySelectorAll(`.cards-${second}`);
 
   for (let i = 0; i < moveScroll.length; i++) {
     const moveScrollIndex = moveScroll[i];
     let calcDistanceTop = moveScrollIndex.getBoundingClientRect().top;
     let calcScreenHeight = window.innerHeight / 1.5;
 
-    moveScrollIndex.classList.add(`time-${number}`);
-
+    moveScrollIndex.classList.add(`time-${second}`);
     if (calcDistanceTop <= calcScreenHeight) {
       moveScrollIndex.classList.add(`move-left-to-right`);
     } else {
@@ -46,3 +39,44 @@ const isTypeTime = (number) => {
     }
   }
 };
+
+/* *************** MODAL SECTION BETWEEN SECTION SERVICES AND PHONE *************** */
+const isOpenModal = (e) => {
+  e.preventDefault();
+
+  modalContainer.style.opacity = "1";
+  modalContainer.style.visibility = "visible";
+  modal.classList.toggle("modal-closed");
+};
+
+const isCloseModal = () => {
+  modal.classList.toggle("modal-close");
+  setTimeout(() => {
+    modalContainer.style.opacity = "0";
+    modalContainer.style.visibility = "hidden";
+  }, 1000);
+};
+
+const isCloseModalTarget = (e) => {
+  if (e.target == modalContainer) {
+    modal.classList.toggle("modal-closed");
+
+    setTimeout(() => {
+      modalContainer.style.opacity = "0";
+      modalContainer.style.visibility = "hidden";
+    }, 1000);
+  }
+};
+
+// -- EVENTS --
+
+document.addEventListener("scroll", () => {
+  isTypeTime("2");
+  isTypeTime("3");
+  isTypeTime("4");
+  isTypeTime("5");
+});
+
+open.addEventListener("click", isOpenModal);
+close.addEventListener("click", isCloseModal);
+document.addEventListener("click", isCloseModalTarget);
