6c06abca90ac984171e560c16bc1264c7a246aa7
Teresa Carbajal <tezzi.cah@gmail.com>
2021-02-22 00:05:31 -0600

    Setup function for event scroll on each element section services to animation enter from right on scroll top

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// -- VARIABLES --
+
 let moveImage = document.querySelector("#moveImage");
 let containerSpinner = document.querySelector("#container-spinner");
+
+// -- EVENTS --
+
+document.addEventListener("scroll", () => {
+  isTypeTime("2");
+  isTypeTime("3");
+  isTypeTime("4");
+  isTypeTime("5");
+});
+
+// -- FUNCTIONS --
 
 /* *************** SPINNER LOADING *************** */
 window.onload = () => {
@@ -13,3 +26,24 @@ setTimeout(() => {
   moveImage.style.visibility = "visible";
   moveImage.classList.add("move-right-to-left");
 }, 2000);
+
+/* *************** SCROLL SECTION SERVICES *************** */
+
+const isTypeTime = (number) => {
+  let moveScroll = document.querySelectorAll(`.cards-${number}`);
+
+  for (let i = 0; i < moveScroll.length; i++) {
+    const moveScrollI = moveScroll[i];
+
+    let calcDistanceTop = moveScrollI.getBoundingClientRect().top;
+    let calcScreenHeight = window.innerHeight / 1.5;
+
+    moveScrollI.classList.add(`time-${number}`);
+
+    if (calcDistanceTop <= calcScreenHeight) {
+      moveScrollI.classList.add(`move-left-to-right`);
+    } else {
+      moveScrollI.classList.remove(`move-left-to-right}`);
+    }
+  }
+};
