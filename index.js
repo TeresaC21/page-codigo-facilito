4ea2a4123dd51b764b5ed733ba7607e0dfc30f6e
Teresa Carbajal <tezzi.cah@gmail.com>
2021-02-21 23:51:21 -0600

    Added file js for spinner animation, preparation for loading

---
+++ index.js
@@ -0,0 +1,8 @@
+let moveImage = document.querySelector("#moveImage");
+let containerSpinner = document.querySelector("#container-spinner");
+
+/* *************** SPINNER LOADING *************** */
+window.onload = () => {
+  containerSpinner.style.visibility = "hidden";
+  containerSpinner.style.opacity = "0";
+};
